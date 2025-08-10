3660ea1778bba09f23b4c6f3945e43ee5dc42ff8
stormriders99 <stormriders99@yahoo.com>
2025-08-10 14:02:31 +0800

    Update q6.py

--- q6.py
+++ q6.py
@@ -19,5 +19,5 @@ def find_first_negative(lst):
 # - [3, 5, -1, 7, -2, 8]
 # - [2, 10, 7, 0]
 
-print(find_first_negative([3,5,-1,7,-2,8])
-print(find_first_negative([2,10,7,0])
+print(find_first_negative([3,5,-1,7,-2,8]))
+print(find_first_negative([2,10,7,0]))
